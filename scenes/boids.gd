649bf167d98242834e38c5cbde25eae3ec801597
kirilbud <76541897+kirilbud@users.noreply.github.com>
2026-03-10 06:30:44 -0700

    yeah this be taken me 20 years of me life

--- scenes/boids.gd
+++ scenes/boids.gd
@@ -3,19 +3,28 @@ extends Node3D
 const boid_res: PackedScene = preload("res://Models/blender/fish.blend")
 
 ####This shit be using the packed vector for performance
+
 @export var num_boids:int = 200
 @export var spawn_radius:float = 20
 @export var boid_vision_radius_squared:float = 9
+@export var boid_speed:float = 1.5
+@export var ray_length:float = 1.5
 
+##steer to avoid crowding local flockmates
 @export_range(0.0, 6.0, 0.01) var rule1_strength: float = 1
+##steer towards the average heading of local flockmates
 @export_range(0.0, 6.0, 0.01) var rule2_strength: float = 1
+##steer to move towards the average position (center of mass) of local flockmates
 @export_range(0.0, 6.0, 0.01) var rule3_strength: float = 1
+##obstical avoidance
 @export_range(0.0, 6.0, 0.01) var rule4_strength: float = 1
-@export_range(0.0, 6.0, 0.01) var rule5_strength: float = 1
+#@export_range(0.0, 6.0, 0.01) var rule5_strength: float = 1
+
+@export var boundary_size: float = 10
 
 var boid_positions := PackedVector3Array()
 var boid_velocity := PackedVector3Array()
-var boid_mesh: Array[Node3D] = Array[Node3D]
+var boid_mesh: Array[Node3D] = []
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
@@ -46,14 +55,17 @@ func _ready():
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta):
+func _physics_process(delta):
 	
-	#todo optimization step where this is called every random range of frames
+	#todo optimization step where this is called every random range of frames (actually unsure how possible this is)
 	#todo octree optimizations
+	#todo multi threading
 	
 	#idk if it matters in godot but im gonna put these here so it avoids alocating and dealocating memory
 	var number_of_boids_near: int = 0;
 	var dist: float = 0;
+	
+	var space_state = get_world_3d().direct_space_state
 	
 	#iterate through all boids
 	for boid1 in range(num_boids):
@@ -67,7 +79,7 @@ func _process(delta):
 		number_of_boids_near = 0
 		
 		#for each boid iterate through that
-		for boid2 in range(num_boids):
+		for boid2 in range(num_boids): #todo also avoid boids behind with dot product
 			
 			if boid1 == boid2:#skip if we would be refering to ourselfs
 				continue
@@ -80,12 +92,54 @@ func _process(delta):
 			
 			
 			#mmk now do some vector math lets a go wahoo! (reference to the game series super mario in case you didnt know)
-			rule1 = rule1 + (boid_positions.get(boid1) - boid_positions.get(boid2))
+			var diff = boid_positions.get(boid1) - boid_positions.get(boid2)
+			var distance = diff.length()
+			if distance > 0.001: 
+				rule1 += (diff.normalized() / distance)
 			
 			rule2 = rule2 + boid_velocity.get(boid2)
 			
 			rule3 = rule3 + boid_positions.get(boid2)
 		
+		#collision detection
+		var start_of_ray = boid_positions.get(boid1)
+		var end_of_ray = boid_positions.get(boid1) + boid_velocity.get(boid1) * ray_length
+		
+		var query = PhysicsRayQueryParameters3D.create(start_of_ray, end_of_ray)
+		var collision = space_state.intersect_ray(query)
+		
+		if !collision.is_empty():
+			var wall_normal = collision.normal
+			var hit_point = collision.position
+			var obstical_distance = boid_positions.get(boid1).distance_to(hit_point)
+			var panic_multiplier = 1 / max(obstical_distance, 0.1) #stronger the closer we are to a wall
+			rule4 = wall_normal * panic_multiplier
+		
+		if number_of_boids_near != 0:#average that shit crazy style
+			rule2 = rule2 / number_of_boids_near
+			rule3 = rule3 / number_of_boids_near
+			
+			rule2 = rule2 - boid_velocity.get(boid1)
+			rule3 = rule3 - boid_positions.get(boid1)
+		
+		var new_boid_velocity = boid_velocity.get(boid1)*2 + (rule1 * rule1_strength) + (rule2 * rule2_strength) + (rule3 * rule3_strength) + (rule4 * rule4_strength)
+		
+		#update velocity and position
+		boid_velocity.set(boid1, new_boid_velocity.normalized() * boid_speed)
+		
+		var new_pos = boid_positions.get(boid1) + boid_velocity.get(boid1) * delta
+		new_pos.x = wrapf(new_pos.x, -boundary_size, boundary_size)
+		new_pos.y = wrapf(new_pos.y, -boundary_size, boundary_size)
+		new_pos.z = wrapf(new_pos.z, -boundary_size, boundary_size)
+		
+		boid_positions.set(boid1, new_pos)
 		
 		
+		
+		#update mesh
+		var look_target =  boid_positions.get(boid1) + boid_velocity.get(boid1)
+		boid_mesh[boid1].position = boid_positions.get(boid1)
+		boid_mesh[boid1].look_at(look_target,Vector3(0,1,0))
+		boid_mesh[boid1].rotate(Vector3(-1,0,0), PI/2)
+		boid_mesh[boid1].rotate(Vector3(0,1,0), PI/2)
 	pass
